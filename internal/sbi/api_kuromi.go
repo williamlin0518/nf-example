9981fe101a0e232b98520b2e328bb55f51743571
Alonza0314 <a098407838@gmail.com>
2024-08-31 09:37:30 -0700

    fix: resolve gci issue

--- internal/sbi/api_kuromi.go
+++ internal/sbi/api_kuromi.go
@@ -9,10 +9,10 @@ import (
 func (s *Server) getKuromiRoute() []Route {
 	return []Route{
 		{
-			Name:		"Hello KUROMI",
-			Method:		http.MethodGet,
-			Pattern:	"/",
-			APIFunc:	func(c *gin.Context) {
+			Name:    "Hello KUROMI",
+			Method:  http.MethodGet,
+			Pattern: "/",
+			APIFunc: func(c *gin.Context) {
 				c.JSON(http.StatusOK, "Hello Kuromi!")
 			},
 			// Use
@@ -24,23 +24,23 @@ func (s *Server) getKuromiRoute() []Route {
 func (s *Server) postKuromiRoute() []Route {
 	return []Route{
 		{
-			Name:		"Big KUROMI",
-			Method:		http.MethodPost,
-			Pattern:	"/",
-			APIFunc:	func(c *gin.Context) {
-				c.String(http.StatusOK, "⣴⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡷\n" +
-				"⠈⣿⣷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣿⣿\n" +
-				"⠀⢸⣿⣿⣿⣿⣷⣆⣀⣀⣀⣀⣀⣾⣿⣿⣿⣿⡇\n" +
-				"⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇\n" +
-				"⠀⠀⠿⢿⣿⣿⣿⣿⡏⡀⠀⡙⣿⣿⣿⣿⣿⠛\n" +
-				"⠀⠀⠀⣿⣿⣿⡿⠟⠷⣅⣀⠵⠟⢿⣿⣿⣿⡆\n" +
-				"⠀⠀⠀⣿⣿⠏⢲⣤⠀⠀⠀⠀⢠⣶⠙⣿⣿⠃\n" +
-				"⠀⠀⠀⠘⢿⡄⠈⠃⠀⢐⢔⠀⠈⠋⢀⡿⠋\n" +
-				"⠀⠀⠀⢀⢀⣼⣷⣶⣤⣤⣭⣤⣴⣶⣍\n" +
-				"⠀⠀⠀⠈⠈⣈⢰⠿⠛⠉⠉⢻⢇⠆⣁⠁\n" +
-				"⠀⠀⠀⠀⠀⠑⢸⠉⠀⠀⠀⠀⠁⡄⢘⣽⣿\n" +
-				"⠀⠀⠀⠀⠀⠀⡜⠀⠀⢰⡆⠀⠀⠻⠛⠋\n" +
-				"⠀⠀⠀⠀⠀⠀⠑⠒⠒⠈⠈⠒⠒⠊")
+			Name:    "Big KUROMI",
+			Method:  http.MethodPost,
+			Pattern: "/",
+			APIFunc: func(c *gin.Context) {
+				c.String(http.StatusOK, "⣴⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡷\n"+
+					"⠈⣿⣷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣿⣿\n"+
+					"⠀⢸⣿⣿⣿⣿⣷⣆⣀⣀⣀⣀⣀⣾⣿⣿⣿⣿⡇\n"+
+					"⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇\n"+
+					"⠀⠀⠿⢿⣿⣿⣿⣿⡏⡀⠀⡙⣿⣿⣿⣿⣿⠛\n"+
+					"⠀⠀⠀⣿⣿⣿⡿⠟⠷⣅⣀⠵⠟⢿⣿⣿⣿⡆\n"+
+					"⠀⠀⠀⣿⣿⠏⢲⣤⠀⠀⠀⠀⢠⣶⠙⣿⣿⠃\n"+
+					"⠀⠀⠀⠘⢿⡄⠈⠃⠀⢐⢔⠀⠈⠋⢀⡿⠋\n"+
+					"⠀⠀⠀⢀⢀⣼⣷⣶⣤⣤⣭⣤⣴⣶⣍\n"+
+					"⠀⠀⠀⠈⠈⣈⢰⠿⠛⠉⠉⢻⢇⠆⣁⠁\n"+
+					"⠀⠀⠀⠀⠀⠑⢸⠉⠀⠀⠀⠀⠁⡄⢘⣽⣿\n"+
+					"⠀⠀⠀⠀⠀⠀⡜⠀⠀⢰⡆⠀⠀⠻⠛⠋\n"+
+					"⠀⠀⠀⠀⠀⠀⠑⠒⠒⠈⠈⠒⠒⠊")
 			},
 			// Use
 			// curl -X POST http://127.0.0.163:8000/kuromi/ -w "\n"
