ce5fa7963ff7e46f003c3918041d87d0b07e97e2
william <illiamw81@gmail.com>
2024-12-29 18:57:45 +0000

    fix: fix linting and test issues

--- internal/sbi/api_hello_test.go
+++ internal/sbi/api_hello_test.go
@@ -1,37 +1,79 @@
-package sbi
+package sbi_test
 
-//nolint:dupl
 import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/andy89923/nf-example/internal/sbi"
+	"github.com/andy89923/nf-example/pkg/factory"
 	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
 )
 
-func TestGetHello(t *testing.T) {
+func Test_getHelloRoutes(t *testing.T) {
 	gin.SetMode(gin.TestMode)
-	w := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(w)
 
-	server := &Server{}
-	server.GetHello(c)
+	mockCtrl := gomock.NewController(t)
+	nfApp := sbi.NewMocknfApp(mockCtrl)
+	nfApp.EXPECT().Config().Return(&factory.Config{
+		Configuration: &factory.Configuration{
+			Sbi: &factory.Sbi{
+				Port: 8000,
+			},
+		},
+	}).AnyTimes()
+	server := sbi.NewServer(nfApp, "")
 
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "Hello from feature/hello!", w.Body.String())
-}
+	t.Run("GetHello", func(t *testing.T) {
+		const EXPECTED_STATUS = http.StatusOK
+		const EXPECTED_BODY = "Hello from feature/hello!"
 
-func TestPostHello(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-	w := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(w)
+		httpRecorder := httptest.NewRecorder()
+		ginCtx, _ := gin.CreateTestContext(httpRecorder)
 
-	c.Params = []gin.Param{{Key: "message", Value: "test"}}
+		var err error
+		ginCtx.Request, err = http.NewRequest("GET", "/", nil)
+		if err != nil {
+			t.Errorf("Failed to create request: %s", err)
+			return
+		}
 
-	server := &Server{}
-	server.PostHello(c)
+		server.GetHello(ginCtx)
 
-	assert.Equal(t, http.StatusOK, w.Code)
-	assert.Equal(t, "Received message: test", w.Body.String())
+		if httpRecorder.Code != EXPECTED_STATUS {
+			t.Errorf("Expected status code %d, got %d", EXPECTED_STATUS, httpRecorder.Code)
+		}
+
+		if httpRecorder.Body.String() != EXPECTED_BODY {
+			t.Errorf("Expected body %s, got %s", EXPECTED_BODY, httpRecorder.Body.String())
+		}
+	})
+
+	t.Run("PostHello", func(t *testing.T) {
+		const EXPECTED_STATUS = http.StatusOK
+		const TEST_MESSAGE = "test"
+		const EXPECTED_BODY = "Received message: test"
+
+		httpRecorder := httptest.NewRecorder()
+		ginCtx, _ := gin.CreateTestContext(httpRecorder)
+
+		var err error
+		ginCtx.Request, err = http.NewRequest("POST", "/"+TEST_MESSAGE, nil)
+		if err != nil {
+			t.Errorf("Failed to create request: %s", err)
+			return
+		}
+		ginCtx.Params = []gin.Param{{Key: "message", Value: TEST_MESSAGE}}
+
+		server.PostHello(ginCtx)
+
+		if httpRecorder.Code != EXPECTED_STATUS {
+			t.Errorf("Expected status code %d, got %d", EXPECTED_STATUS, httpRecorder.Code)
+		}
+
+		if httpRecorder.Body.String() != EXPECTED_BODY {
+			t.Errorf("Expected body %s, got %s", EXPECTED_BODY, httpRecorder.Body.String())
+		}
+	})
 }
